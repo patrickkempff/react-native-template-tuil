d31181dcbc5a78696c83b2f530810331ac29a762
Arjan van Maren <Arjan.vanmaren@weareyou.com>
2020-02-27 14:49:37 +0100

    Initial commit for the upgrade. Android debug keystore doesn't work yet

--- template/android/app/src/main/java/com/projectname/MainApplication.java
+++ template/android/app/src/main/java/com/projectname/MainApplication.java
@@ -1,13 +1,13 @@
 package com.projectname;
 
 import android.app.Application;
-
+import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
